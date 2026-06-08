7d0ca25abb8341cd6585ec35aeb380145e226e55
Tjalle <noreply@tjalle.nl>
2026-06-08 21:03:13 +0200

    Refactor main function and add fallback route for improved error handling

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,25 @@
-use axum::{Router, routing::get};
+use axum::{
+    Router,
+    http::{StatusCode, Uri},
+    routing::get,
+};
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/", get(|| async { "Hello, World!" }));
+    let app = create_router();
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     axum::serve(listener, app).await.unwrap();
+}
+
+async fn fallback_route(uri: Uri) -> (StatusCode, String) {
+    (StatusCode::NOT_FOUND, format!("Route {uri} not found!"))
+}
+
+fn create_router() -> Router<()> {
+    const HEALTHY_RESPONSE: &str = "{\"healthy\": true}";
+
+    Router::<()>::new()
+        .route("/health", get(|| async { HEALTHY_RESPONSE }))
+        .fallback(fallback_route)
 }
