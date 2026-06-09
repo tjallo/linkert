81cdee548d2b8ab09f68242e6bfaac9c5de8fb10
Tjalle <noreply@tjalle.nl>
2026-06-10 00:21:47 +0200

    Add get_first_user_route to helpers and update router configuration

--- src/routes/helpers.rs
+++ src/routes/helpers.rs
@@ -1,4 +1,9 @@
-use axum::http::{StatusCode, Uri};
+use axum::{
+    extract,
+    http::{StatusCode, Uri},
+};
+
+use crate::{app_state::AppState, db::repositories::user::get_first_user};
 
 pub async fn get_health() -> (StatusCode, String) {
     (StatusCode::OK, String::from("{\"healthy\": true}"))
@@ -6,4 +11,13 @@ pub async fn get_health() -> (StatusCode, String) {
 
 pub async fn fallback_route(uri: Uri) -> (StatusCode, String) {
     (StatusCode::NOT_FOUND, format!("Path {uri} not found!"))
+}
+
+// TODO: Remove
+pub async fn get_first_user_route(
+    extract::State(state): extract::State<AppState>,
+) -> (StatusCode, String) {
+    let user = get_first_user(state).await.unwrap();
+
+    (StatusCode::OK, format!("{user:?}"))
 }
--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -11,7 +11,7 @@ use crate::{
     AppState,
     routes::{
         auth::register,
-        helpers::{fallback_route, get_health},
+        helpers::{fallback_route, get_first_user_route, get_health},
     },
 };
 
@@ -19,6 +19,8 @@ pub fn create_router(state: AppState) -> Router<()> {
     Router::<AppState>::new()
         .route("/health", get(get_health))
         .route("/auth/register", post(register))
+        // TODO: Remove
+        .route("/getfirst", get(get_first_user_route))
         .fallback(fallback_route)
         .with_state(state)
 }
