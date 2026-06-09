e079bc4d256f3210ea7b8943196655a6a7205b4b
Tjalle <noreply@tjalle.nl>
2026-06-09 23:26:26 +0200

    Fix SQL syntax in URL migration and ensure proper function definition for user deletion trigger

--- migrations/20260609202002_create_urls.sql
+++ migrations/20260609202002_create_urls.sql
@@ -2,25 +2,25 @@ CREATE TABLE urls (
     id BIGSERIAL PRIMARY KEY,
     stub VARCHAR(32) NOT NULL UNIQUE,
     original_url TEXT NOT NULL,
-    user_id BIGINT REFERENCES users(id) NOT NULL,
+    user_id BIGINT NOT NULL REFERENCES users(id),
     created_at TIMESTAMP DEFAULT NOW(),
     invalid_by TIMESTAMP
 );
 
-CREATE FUNCTION update_invalid_by_after_user_delete()
+CREATE OR REPLACE FUNCTION update_invalid_by_after_user_delete()
     RETURNS TRIGGER
-    LANGUAGE 'plpgsql'
-AS $BODY$
+    LANGUAGE plpgsql
+AS $$
 BEGIN
     UPDATE urls
     SET invalid_by = NOW()
     WHERE user_id = OLD.id;
     RETURN OLD;
-END;
-$BODY$;
+END
+$$;
 
 CREATE TRIGGER after_user_delete
     AFTER DELETE
     ON users
     FOR EACH ROW
-    EXECUTE PROCEDURE update_invalid_by_after_user_delete;
+    EXECUTE PROCEDURE update_invalid_by_after_user_delete();
