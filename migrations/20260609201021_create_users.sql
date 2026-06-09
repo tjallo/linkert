0459da2f48d5495b48d15fa39eae13af7c0e7d05
Tjalle <noreply@tjalle.nl>
2026-06-09 22:15:42 +0200

    Add users table migration with necessary fields

---
+++ migrations/20260609201021_create_users.sql
@@ -0,0 +1,7 @@
+CREATE TABLE users (
+    id BIGSERIAL PRIMARY KEY,
+    username VARCHAR(64) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP DEFAULT NOW(),
+    updated_at TIMESTAMP
+)
