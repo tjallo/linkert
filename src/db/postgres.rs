aee4e9b2613c366eca89c474b135085e599da1c2
Tjalle <noreply@tjalle.nl>
2026-06-09 22:33:44 +0200

    Make postgres module public and implement connection function in main

--- src/db/postgres.rs
+++ src/db/postgres.rs
@@ -1,1 +1,8 @@
+use sqlx::{PgPool, Pool, Postgres};
 
+pub async fn connect_postgres(database_url: &str) -> Pool<Postgres> {
+    match PgPool::connect(database_url).await {
+        Ok(pool) => pool,
+        Err(err) => panic!("Failed to connect to Postgres Pool: {err:?}"),
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,11 @@ mod models;
 mod routes;
 mod server;
 
-use crate::{config::load_dotenv, db::redis::connect_redis, server::start_webserver};
+use crate::{
+    config::load_dotenv,
+    db::{postgres::connect_postgres, redis::connect_redis},
+    server::start_webserver,
+};
 
 #[tokio::main]
 async fn main() {
@@ -13,6 +17,7 @@ async fn main() {
 
     let config = load_dotenv();
     let mut _redis_con = connect_redis(&config.redis_url);
+    let _postgres_con = connect_postgres(&config.database_url);
 
     start_webserver().await
 }
