32b255b283f2f5b49bb1d70c2f779f64ba69be00
Tjalle <noreply@tjalle.nl>
2026-06-09 23:43:03 +0200

    Implement password hashing and verification using Argon2

--- src/auth/password.rs
+++ src/auth/password.rs
@@ -1,1 +1,25 @@
+use argon2::{
+    Argon2, PasswordHash, PasswordVerifier,
+    password_hash::{PasswordHasher, SaltString},
+};
+use rand::rngs::OsRng;
 
+pub fn hash_password(password: &str) -> String {
+    let password = password.as_bytes();
+    let salt = SaltString::generate(&mut OsRng);
+
+    let argon2 = Argon2::default();
+
+    match argon2.hash_password(password, &salt) {
+        Ok(hash) => hash.to_string(),
+        Err(err) => panic!("{err:?}"),
+    }
+}
+
+pub fn verify_password(password: &str, hash: &str) -> bool {
+    let parsed_hash = PasswordHash::new(&hash).unwrap();
+
+    Argon2::default()
+        .verify_password(password.as_bytes(), &parsed_hash)
+        .is_ok()
+}
